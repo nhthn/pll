9852fd785c940c827a390e315b55e86c8c44c123
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-09-24 13:53:27 -0700

    Add pll.hpp

---
+++ pll.hpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <cmath>
+
+class PLL {
+public:
+    PLL(float sampleRate) : m_sampleRate(sampleRate) {
+    }
+
+    enum class PhaseDetectorAlgorithm {
+        Multiplier,
+        JKFlipFlop,
+        PFD
+    };
+
+    void setLPFK(float k) {
+        m_lpfK = k;
+    }
+
+    void setVCOCenterFrequency(float vcoCenterFrequency) {
+        m_vcoCenterFrequency = vcoCenterFrequency;
+    }
+
+    void setVCOFrequencyGain(float vcoFrequencyGain) {
+        m_vcoFrequencyGain = vcoFrequencyGain;
+    }
+
+    void setPhaseDetectorAlgorithm(PhaseDetectorAlgorithm phaseDetectorAlgorithm) {
+        m_phaseDetectorAlgorithm = phaseDetectorAlgorithm;
+    }
+
+    float process(float in) {
+        float phaseDetectorOut;
+        switch (m_phaseDetectorAlgorithm) {
+        case PhaseDetectorAlgorithm::JKFlipFlop:
+            if (m_inLast <= 0 && in > 0) {
+                m_jkFlipFlopOut = 1;
+            }
+            if (m_vcoLast2 <= 0 && m_vcoLast > 0) {
+                m_jkFlipFlopOut = -1;
+            }
+            phaseDetectorOut = m_jkFlipFlopOut;
+            break;
+        case PhaseDetectorAlgorithm::PFD:
+            if (m_inLast <= 0 && in > 0) {
+                m_pfdState = std::min(m_pfdState + 1, 1);
+            }
+            if (m_vcoLast2 <= 0 && m_vcoLast > 0) {
+                m_pfdState = std::max(m_pfdState - 1, -1);
+            }
+            phaseDetectorOut = m_pfdState;
+            break;
+        default:
+            phaseDetectorOut = m_vcoLast * in;
+            break;
+        }
+        m_inLast = in;
+        m_vcoLast2 = m_vcoLast;
+
+        float lpfOut = phaseDetectorOut * (1 - m_lpfK) + m_lpfLast * m_lpfK;
+        m_lpfLast = lpfOut;
+        float vcoFrequency = m_vcoCenterFrequency + lpfOut * m_vcoFrequencyGain;
+        float vcoPhaseIncrement = vcoFrequency / m_sampleRate;
+        m_vcoPhase = std::fmod(m_vcoPhase + vcoPhaseIncrement, 1.0f);
+        float vcoOut = m_vcoPhase < 0.5 ? 1 : -1;
+        m_vcoLast = vcoOut;
+        return vcoOut;
+    }
+
+private:
+    const float m_sampleRate;
+
+    float m_lpfK = 0.999;
+    float m_vcoCenterFrequency = 440;
+    float m_vcoFrequencyGain = 100;
+
+    PhaseDetectorAlgorithm m_phaseDetectorAlgorithm = PhaseDetectorAlgorithm::Multiplier;
+
+    float m_inLast = 0;
+    float m_vcoLast2 = 0;
+    int m_jkFlipFlopOut = 0;
+    int m_pfdState = 0;
+
+    float m_vcoLast = 0;
+    float m_vcoPhase = 0;
+    float m_lpfLast = 0;
+};
